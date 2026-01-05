6b8357f2dfb8b203bfe4d7ced448f9e0020cf041
Jolly Rodgers <34277247+twcg@users.noreply.github.com>
2026-01-05 13:11:14 -0500

    Delete cloud-custom-rules directory

--- cloud-custom-rules/scripts/test-policies.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-conftest test infra/examples/bad_s3/plan.json -p policy/opa --parser json --all-namespaces
-conftest test infra/examples/good_s3/plan.json -p policy/opa --parser json --all-namespaces
